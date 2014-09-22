053328068f9c1bf738a02e0a14ffad0c30be658e
marco30 <mv222fp@student.lnu.se>
2014-09-22 15:29:47 +0200

    'lab.1.3'

--- 1DV402.S1.3.L01A/1DV402.S1.3.L01A/Program.cs
+++ 1DV402.S1.3.L01A/1DV402.S1.3.L01A/Program.cs
@@ -11,49 +11,64 @@ namespace _1DV402.S1._3.L01A
         static void Main(string[] args)
         {
 
-            int antallöner = 0;
+            int antalloner = 0;
 
             int exit = 0;
 
-            while(exit == 2)
+            while(exit < 2)
             {
 
-                while(true) // in matning av anntal löner som ska skrivas in händer i den här loppen
-                {
-                    try // öppnar up möjligheten att hantera fel chatch som kan uppstå i programmet
-                    {
-                        
-                        Console.Write("Antal löner att matta in: ");
-                        antallöner = int.Parse(Console.ReadLine());
+                    antalloner = ReadInt();
 
-                        if (antallöner <= 1) // för att systemet ska funka måste man minst mata in 2 personers lön, gör man inte det så dyker det här fel meddelandet upp 
-                        {
-                            Console.BackgroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Du måste mata in minst två löner för att kunna göra en bräkning!");
-                            Console.ResetColor();
-                        }
-                        else // är det mer än en krona avlutat loppen med break
-                        {
-                            break;
-                        }
-                    }
-                    catch // skriver man en bokstav, punkt eller något som inte uppfattas som ett tal så visas det här
-                    {
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.WriteLine("det är inte en siffra");
-                        Console.ResetColor();
-                    } 
+                    Console.WriteLine(antalloner); 
+                   Console.ReadKey();
 
-                }
-
-
-
-
+                   exit++;
 
             }
 
-
-
         }
+
+        static int ReadInt()
+        {
+            int ReadInt;
+
+            while(true)
+            {
+
+           
+            try // öppnar up möjligheten att hantera fel chatch som kan uppstå i programmet
+            {
+
+                Console.Write("Antal löner att matta in: ");
+                ReadInt = int.Parse(Console.ReadLine());
+
+                if (ReadInt <= 1) // för att systemet ska funka måste man minst mata in 2 personers lön, gör man inte det så dyker det här fel meddelandet upp 
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du måste mata in minst två löner för att kunna göra en bräkning!");
+                    Console.ResetColor();
+                }
+                else // har man mattat in minst 2 personers så abryts while satsen 
+                {
+                    break;
+                }
+
+            }
+
+            catch // skriver man en bokstav, punkt eller något som inte uppfattas som ett tal så visas det här
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("det är inte en siffra");
+                Console.ResetColor();
+            }
+
+            
+            }
+
+            return ReadInt;
+        }  
+
+
     }
 }
